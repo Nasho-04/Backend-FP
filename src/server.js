ce3985eb00dc627bfcf526cef07a4037a44c8965
Nacho <nachonieva04@gmail.com>
2024-12-07 15:39:27 -0300

    Test

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import ENVIRONMENT from './config/environment.js'
-import authRouter from './Routes/auth.router.js'
+import authRouter from './routes/auth.router.js'
 import productRouter from './routes/product.router.js'
 import mongoose from './db/config.js'
 import cors from 'cors'
