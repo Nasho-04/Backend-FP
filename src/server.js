ed299a3246d1ae4e9043bf25873ea7a67eaebe0a
Nacho <nachonieva04@gmail.com>
2024-12-10 20:02:36 -0300

    ectended the limit of my responses

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ import mongoose from './db/config.js'
 import cors from 'cors'
 
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({extended: true, limit: '50mb'}))
 app.use(cors())
 
 
