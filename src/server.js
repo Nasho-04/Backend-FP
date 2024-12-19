0fcfc8f20a09d19333951747775f8def3568feb8
Nacho <nachonieva04@gmail.com>
2024-12-19 13:49:50 -0300

    .

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended: true, limit: '50mb'}))
 app.use(cors(
     {
-        origin: '*',
+        AccessControlAllowOrigin: '*'
     }
 ))
 
