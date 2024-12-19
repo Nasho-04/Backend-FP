dc865b2246371eae2d7747d4111df99b333434c6
Nacho <nachonieva04@gmail.com>
2024-12-19 15:03:48 -0300

    got rid of cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import cors from 'cors'
 import cartRouter from './rutas/cart.router.js'
 
 const app = express()
-app.use(cors())
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended: true, limit: '50mb'}))
 app.use((req, res, next) => {
