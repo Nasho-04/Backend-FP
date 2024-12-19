1c52b66beee653535b511c57a81c3bafbb110aae
Nacho <nachonieva04@gmail.com>
2024-12-19 16:34:06 -0300

    adding a middleware

--- src/rutas/cart.router.js
+++ src/rutas/cart.router.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { getCartController, addToCartController } from '../controllers/cartController.js'
+import { verifyApikeyMiddleware, verifyTokenMiddleware } from '../middlewares/auth.middlewares.js'
 
 const cartRouter = express.Router()
 
 cartRouter.get('/', getCartController)
 
-cartRouter.post('/:product_id', addToCartController)
+cartRouter.post('/:product_id', verifyTokenMiddleware([]), addToCartController)
 
 export default cartRouter
