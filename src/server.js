eacab7544f9d1f25f1f3d294f423bf05c74bfeaa
Nacho <nachonieva04@gmail.com>
2024-12-19 14:33:27 -0300

    put my frontend url as cors origin

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended: true, limit: '50mb'}))
 app.use(cors(
     {
-        AccessControlAllowOrigin: '*'
+        AccessControlAllowOrigin: 'https://frontend-fp-two.vercel.app'
     }
 ))
 
