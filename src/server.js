1b77342974a63df878bad074a225b04dba94d4b7
Nacho <nachonieva04@gmail.com>
2024-12-19 13:44:23 -0300

    added cors configuration

--- src/server.js
+++ src/server.js
@@ -10,6 +10,13 @@ const app = express()
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended: true, limit: '50mb'}))
 app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+})
 
 
 app.use('/api/auth', authRouter)
