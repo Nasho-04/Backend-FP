60731c7a8432f60c77e854bbe72249d6c15f1775
Nacho <nachonieva04@gmail.com>
2024-12-19 14:34:17 -0300

    .

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended: true, limit: '50mb'}))
 app.use(cors(
     {
-        AccessControlAllowOrigin: 'https://frontend-fp-two.vercel.app'
+        AccessControlAllowOrigin: '*'
     }
 ))
 
